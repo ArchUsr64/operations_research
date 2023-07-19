b5a8df0d438e245664ca53764a830c92ea7c4591
ArchUsr64 <anshulusr@gmail.com>
2023-07-19 12:45:55 +0530

    added basic project structure

---
+++ linear_programming/src/graphical.rs
@@ -0,0 +1,5 @@
+//! Finds solution to a linear programming problem using graphical method
+//! This method only works on with two decision variables
+fn main() {
+    todo!("Implement graphical solution parsing");
+}
