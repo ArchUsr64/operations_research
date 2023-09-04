e3716484d5af72a0f27ef3fa05f714d967a17ff8
Anshul <anshulusr@gmail.com>
2023-09-04 20:47:42 +0530

    added assignemnt model project

---
+++ assignment_model/src/main.rs
@@ -0,0 +1,10 @@
+const N: usize = 4;
+const C: [[usize; N]; N] = [[5, 9, 3, 6], [8, 7, 8, 2], [6, 10, 12, 7], [3, 10, 8, 6]];
+
+/// The assignment problem goes as follows:
+/// Given N jobs and N machines where each machine can only perform a single task,
+/// find the allocation of jobs that minimizes the cost given an NxN matrix with
+/// Cost C_ij corresponding to the cost taken by Machine j to do Job i
+fn main() {
+    todo!()
+}
